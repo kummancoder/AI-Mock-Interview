06bfdc2444c82b65d06a2ce7a17f4bf49a3f4335
RaviD98 <notravi98@gmail.com>
2025-04-07 18:11:28 +0530

    Changes : Home Screen Completed

--- src/handlers/auth-handler.tsx
+++ src/handlers/auth-handler.tsx
@@ -9,39 +9,55 @@ import { useLocation, useNavigate } from "react-router-dom";
 const AuthHandler = () => {
   const { isSignedIn } = useAuth();
   const { user } = useUser();
+  // const { isSignedIn, isLoaded: authLoaded } = useAuth();
+  // const { user, isLoaded: userLoaded } = useUser();
+
 
   const pathname = useLocation().pathname;
   const navigate = useNavigate();
 
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const storeUserData = async () => {
-      if (isSignedIn && user) {
-        setLoading(true);
-        try {
-          const userSnap = await getDoc(doc(db, "users", user.id));
-          if (!userSnap.exists()) {
-            const userData: User = {
-              id: user.id,
-              name: user.fullName || user.firstName || "Anonymous",
-              email: user.primaryEmailAddress?.emailAddress || "N/A",
-              imageUrl: user.imageUrl,
-              createdAt: serverTimestamp(),
-              updateAt: serverTimestamp(),
-            };
+ useEffect(() => {
+   const storeUserData = async () => {
 
-            await setDoc(doc(db, "users", user.id), userData);
-          }
-        } catch (error) {
-          console.log("Error on storing the user data : ", error);
-        } finally {
-          setLoading(false);
-        }
-      }
-    };
-    storeUserData();
-  }, [isSignedIn, user, pathname, navigate]);
+      if (isSignedIn && user) {
+       setLoading(true);
+       try {
+         const userSnap = await getDoc(doc(db, "users", user.id));
+         console.log("Document exists?", userSnap.exists());
+
+         if (!userSnap.exists()) {
+           const userData: User = {
+             id: user.id,
+             name: user.fullName || user.firstName || "Anonymous",
+             email: user.primaryEmailAddress?.emailAddress || "N/A",
+             imageUrl: user.imageUrl,
+             createdAt: serverTimestamp(),
+             updateAt: serverTimestamp(),
+           };
+
+          //  console.log("Creating user doc:", userData);
+           await setDoc(doc(db, "users", user.id), userData);
+           console.log("User document created.");
+         } 
+        //  else {
+        //    console.log("User already exists, skipping creation.");
+        //  }
+       } catch (error) {
+         console.log("Error on storing the user data:", error);
+       } finally {
+         setLoading(false);
+       }
+     } else {
+       console.log("Waiting for auth to load or user not signed in.");
+     }
+   };
+
+   storeUserData();
+ }, [isSignedIn, user, pathname, navigate]);
+
+
 
   if (loading) {
     return <LoaderPage />;
